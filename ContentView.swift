8308714cbcaf97a72c4a13a50d7bf69388548243
Travis Wentz <twentz@avanleecare.com>
2021-03-19 15:01:24 -0600

    first commit

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Board Foot Calculator
+//
+//  Created by Work on 2/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
